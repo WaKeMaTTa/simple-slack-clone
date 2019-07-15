ddbe012675f2a9a928b219b436f987d13158595e
Mohamed Ziata <wakematta@gmail.com>
2019-07-15 13:11:13 +0200

    :building_construction: Initial structure of the App

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react"
+// import MessageList from "./componenets/MessageList"
+// import SendMessageForm from "./componenets/SendMessageForm"
+// import ChannelList from "./componenets/ChannelList"
+// import NewChannelForm from "./componenets/NewChannelForm"
+
+class App extends React.Componenet {
+  render() {
+    return (
+      <div className="app">
+        // <ChannelList />
+        // <MessageList />
+        // <SendMessageForm />
+        // <NewChannelForm />
+      </div>
+    )
+  }
+}
+
+export default App
