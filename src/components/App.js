8d21e4a42a618d6c202d9d86950f89d3df6be51a
Mohamed Ziata <wakematta@gmail.com>
2019-07-16 15:13:38 +0200

    :art: Connect to room and recive message

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
+import { tokenUrl, instanceLocator } from '../config'
+
 import React from "react"
+import Chatkit from "@pusher/chatkit-client"
 
 import MessageList from "./MessageList"
 import SendMessageForm from "./SendMessageForm"
@@ -6,6 +9,34 @@ import ChannelList from "./ChannelList"
 import NewChannelForm from "./NewChannelForm"
 
 class App extends React.Component {
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: instanceLocator,
+      userId: 'wakematta',
+      tokenProvider: new Chatkit.TokenProvider({
+        url: tokenUrl
+      })
+    })
+
+    chatManager
+      .connect()
+      .then(currentUser => {
+        console.log('Successful connection', currentUser)
+        currentUser.subscribeToRoom({
+          roomId: '21655498',
+          messageLimit: 20,
+          hooks: {
+            onMessage: message => {
+              console.log('message.text: ', message.text)
+            }
+          }
+        })
+      })
+      .catch(err => {
+        console.log('Error on connection', err)
+      })
+  }
+
   render() {
     return (
       <div className="app">
