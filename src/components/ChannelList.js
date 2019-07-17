9691579f963f207c487e3f358ad9c4c6f4467663
Mohamed Ziata <wakematta@gmail.com>
2019-07-17 16:19:08 +0200

    :art: Display the list of channels with the same order always

--- src/components/ChannelList.js
+++ src/components/ChannelList.js
@@ -3,12 +3,15 @@ import React from "react"
 class ChannelList extends React.Component {
   render() {
     // console.log(this.props.channels)
+
+    const orderedChannels = [...this.props.channels].sort((a, b) => a.id - b.id)
+
     return (
       <div className="channels-list">
         <h3>Channels</h3>
         <ul>
           {
-            this.props.channels.map(channel => {
+            orderedChannels.map(channel => {
               return (
                 <li key={channel.id} className="channel">
                   <a onClick={() => this.props.subscribeToChannel(channel.id)}
